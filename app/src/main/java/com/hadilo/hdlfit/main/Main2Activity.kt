df8d67b3811a12badb80d06ecea8db87a17d6eb3
hadilo <devhadi@gmail.com>
2019-07-20 23:07:16 +0700

    add popup input movement name

--- app/src/main/java/com/hadilo/hdlfit/main/Main2Activity.kt
+++ app/src/main/java/com/hadilo/hdlfit/main/Main2Activity.kt
@@ -2,8 +2,12 @@ package com.hadilo.hdlfit.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TextInputLayout
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
 import com.hadilo.hdlfit.R
 import com.hadilo.hdlfit.inputData.InputDataActivity
 import com.hadilo.hdlfit.model.DataModel
@@ -21,7 +25,11 @@ class Main2Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {
+        fab_movement_name.setOnClickListener {
+            popup()
+        }
+
+        fab_progress.setOnClickListener {
             val intent = Intent(this, InputDataActivity::class.java)
             startActivityForResult(intent, INSERT_REQUEST)
         }
@@ -39,4 +47,31 @@ class Main2Activity : AppCompatActivity() {
             }
         }
     }
+
+    fun popup() {
+        val inflater = LayoutInflater.from(this)
+        val view = inflater.inflate(R.layout.dialog_input_movement_name, null)
+
+        val builder = AlertDialog.Builder(this)
+            .setView(view)
+            .show()
+
+        val tilMovementName = view.findViewById<TextInputLayout>(R.id.til_movement_name)
+        val btnSave = view.findViewById<Button>(R.id.btn_save)
+
+        btnSave.setOnClickListener {
+            if (validasiPopup(tilMovementName.editText?.text.toString())) {
+                builder.dismiss()
+            }
+        }
+    }
+
+    fun validasiPopup(movementName: String?): Boolean{
+        if(movementName.isNullOrEmpty()) {
+//            showDialogError("Bandara Keberangkatan harus diisi");
+            return false
+        }
+        return true
+    }
+
 }
