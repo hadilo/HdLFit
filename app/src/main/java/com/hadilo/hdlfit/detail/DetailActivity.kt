674038fc2dd6ed2f3df972c6fc2535d75f8fce1d
hadilo <devhadi@gmail.com>
2019-08-11 08:07:34 +0700

    add Detail Activity

---
+++ app/src/main/java/com/hadilo/hdlfit/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.hadilo.hdlfit.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hadilo.hdlfit.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
