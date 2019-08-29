ec3dfe777dfc094b21c8ee14950f24f5a55818b8
hadilo <devhadi@gmail.com>
2019-08-29 22:43:37 +0700

    remove unused code

--- app/src/main/java/com/hadilo/hdlfit/detail/DetailPresenter.kt
+++ app/src/main/java/com/hadilo/hdlfit/detail/DetailPresenter.kt
@@ -1,16 +1,6 @@
 package com.hadilo.hdlfit.detail
 
-import android.util.Log
-import com.backendless.Backendless
-import com.backendless.BackendlessUser
-import com.backendless.async.callback.AsyncCallback
-import com.backendless.exceptions.BackendlessFault
-import com.backendless.persistence.DataQueryBuilder
-import com.backendless.persistence.local.UserTokenStorageFactory
 import com.hadilo.hdlfit.base.BasePresenter
-import com.hadilo.hdlfit.model.DataModel
-import com.hadilo.hdlfit.model.Movement
-import com.hadilo.hdlfit.model.Property
 
 /**
  * Created by Hadilo Muhammad on 2019-07-21.
