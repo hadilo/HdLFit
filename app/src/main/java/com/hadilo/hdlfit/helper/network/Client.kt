314c94ba69fcae2f8e0458b0a33bc93d9cbd5dc3
hadilo <devhadi@gmail.com>
2019-12-01 16:11:15 +0700

    add interceptor and set RTO

--- app/src/main/java/com/hadilo/hdlfit/helper/network/Client.kt
+++ app/src/main/java/com/hadilo/hdlfit/helper/network/Client.kt
@@ -1,18 +1,40 @@
 package com.hadilo.hdlfit.helper.network
 
 import com.hadilo.hdlfit.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Hadilo Muhammad on 2019-10-06.
+ */
 
 class Client {
 
     val BASE_URL = "https://api.backendless.com/${BuildConfig.BACKENDLESS_APPLICATION_ID}/${BuildConfig.BACKENDLESS_API_KEY}/"
 
     fun getData(): Retrofit {
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptorLoggingEndPoint())
+            .connectTimeout(10, TimeUnit.MINUTES)
+            .readTimeout(10, TimeUnit.MINUTES)
+            .writeTimeout(10, TimeUnit.MINUTES)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
         return retrofit
+    }
+
+    private fun interceptorLoggingEndPoint(): HttpLoggingInterceptor{
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
     }
 }
